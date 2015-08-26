10c54aff254bd014eb4f7f5b5a7b2759d694bb85
Hugo Melo <hugo@riseup.net>
2015-08-26 17:40:21 -0300

    Remove old finds from main

--- src/cake/app/controllers/main_controller.php
+++ src/cake/app/controllers/main_controller.php
@@ -7,43 +7,16 @@ class MainController extends RedeAppController
 	
 	function index()
 	{
-		// GET HIGHLIGHTED CONTENT (3 FIRST COLUMNS)
-		$this->loadModel('MexcHighlights.MexcHighlightedContent');
-		$highlighted = $this->MexcHighlightedContent->getHightlightedsFrom('museu');
-		
-		// GET NEWS
-		$this->loadModel('MexcNews.MexcNew');
-		$this->MexcNew->setActiveStatuses(array('display_level' => array('general')));
-		$news = $this->MexcNew->find('all', array('limit' => 10, 'contain' => false));
-		$three_news = array_slice($news, 0, 3);
-		$seven_news = array_slice($news, 3);
+		// GET GALLERY
+		$this->loadModel('UnifiedSearch.SblSearchItem');
+		$paging = array();
+		$items = $this->SblSearchItem->getSearchResults("", array(), $paging);
 		
 		// GET PROGRAMS
 		$this->loadModel('SiteFactory.FactSite');
 		$fact_sites = $this->FactSite->find('all', array('contain' => false));
 		
-		// GET EVENTS
-		$this->loadModel('MexcEvents.MexcEvent');
-		$this->MexcEvent->setActiveStatuses(array('display_level' => array('general')));
-		$events = $this->MexcEvent->find('all', array(
-			'limit' => 9, 
-			'contain' => false, 
-			'conditions' => array('MexcEvent.end >=' => date('Y-m-d')),
-			'order' => array('MexcEvent.start' => 'asc')
-		));
-		if (empty($events))
-		{
-			$events = $this->MexcEvent->find('all', array('limit' => 9, 'contain' => false));
-		}
-		$two_events = array_slice($events, 0, 2);
-		$seven_events = array_slice($events, 2);
-		
-		// GET GALLERY
-		$this->loadModel('MexcGalleries.MexcGallery');
-		$this->MexcGallery->setActiveStatuses(array('display_level' => array('general')));
-		$gallery = $this->MexcGallery->find('first', array('contain' => array('MexcImage' => array('limit' => 1))));
-		
 		// SET THEM ALL
-		$this->set(compact('highlighted', 'three_news', 'seven_news', 'two_events', 'seven_events', 'gallery', 'fact_sites'));
+		$this->set(compact('items', 'fact_sites'));
 	}
 }
