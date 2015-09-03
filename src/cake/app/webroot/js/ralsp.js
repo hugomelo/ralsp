825397c003bc416a3b28eb2003944bea3851854c
Hugo Melo <hugo@riseup.net>
2015-09-02 22:25:14 -0300

    Create mexc_event index page

--- src/cake/app/webroot/js/ralsp.js
+++ src/cake/app/webroot/js/ralsp.js
@@ -20,12 +20,33 @@ var ralsp = {
 	countLetters: function(event) {
 		console.log($(this).value.length);
 		$('chars-counter').update($(this).value.length + " caracteres");
-	}
+	},
+
+	showEventPage: function(event) {
+		if (event)
+			event.preventDefault();
+
+		var active = jQuery(this).hasClass('active');
+		if (!active) {
+			jQuery(this).siblings().removeClass('active');
+			jQuery(this).addClass('active');
+
+			if (jQuery(this).hasClass('newer')) {
+				jQuery('.agenda .incoming').slideDown();
+				jQuery('.agenda .occurred').slideUp();
+			} else {
+				jQuery('.agenda .incoming').slideUp();
+				jQuery('.agenda .occurred').slideDown();
+			}
+
+		}
+	},
 };
 
 (function($) {
 	$(function() {
 		$('.row.home .projects .project-select a').on('click',ralsp.activateProjectEntry);
 		$('.summary').bind('input propertychange', ralsp.countLetters);
+		$('.agenda.menu .newer, .agenda.menu .older').on('click', ralsp.showEventPage);
 	});
 })(jQuery);
